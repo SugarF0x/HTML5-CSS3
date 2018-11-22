b1e8d7c78345a4053863373ec98448673f7f9600
SugarF0x <malion.torchwood@yandex.ru>
2018-11-22 18:27:05 +0300

    clone of lesson-2

---
+++ index.php
@@ -0,0 +1,251 @@
+<!DOCTYPE html>
+<html lang="ru">
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="styles/style.css">
+        <title>Макет переделанный</title>
+    </head>
+
+    <body>
+        <?php require('req/header.php'); ?>
+        
+        <main>
+            <section>
+               <div id="aboutImage">
+                    <!-- A placeholder for the image -->
+                </div>
+                
+                <h1 style="text-align: left;">About yor product</h1>
+                
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae illum eum repellendus consequatur quasi. Quae dolorum illum, porro aut tenetur. Animi laborum, aspernatur optio molestiae, cupiditate error porro sint quae quos ea iure mollitia rerum sapiente ullam quasi neque exercitationem!
+                </p>
+                
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat modi facilis, magni maiores aperiam assumenda nemo aut voluptatem qui repellendus!
+                </p>
+                
+                <div class="cls"></div>
+            </section>
+                
+            <section id="pluses">
+                <h1>Dignity and pluses</h1>
+                
+                <ul>
+                    <li>
+                        <img src="img/Layer%202.png" alt="plus">
+                        
+                        <p>
+                            Delectus dolorem vero quae beatae quasi dolor deserunt iste amet atque, impedit iure placeat, ullam. Reprehenderit aliquam, nemo cum velit ratione perferendis quas, maxime, quaerat porro totam, dolore.
+                        </p>
+                    </li>
+                    <li><img src="img/Layer%202.png" alt="plus">
+                        
+                        <p>
+                            Delectus dolorem vero quae beatae quasi dolor deserunt iste amet atque, impedit iure placeat, ullam. Reprehenderit aliquam, nemo cum velit ratione perferendis quas, maxime, quaerat porro totam, dolore.
+                        </p>
+                    </li>
+                    <li><img src="img/Layer%202.png" alt="plus">
+                        
+                        <p>
+                            Delectus dolorem vero quae beatae quasi dolor deserunt iste amet atque, impedit iure placeat, ullam. Reprehenderit aliquam, nemo cum velit ratione perferendis quas, maxime, quaerat porro totam, dolore.
+                        </p>
+                    </li>
+                </ul>
+                
+                <ul>
+                    <li>
+                        <img src="img/Layer%202.png" alt="plus">
+                        
+                        <p>
+                            Delectus dolorem vero quae beatae quasi dolor deserunt iste amet atque, impedit iure placeat, ullam. Reprehenderit aliquam, nemo cum velit ratione perferendis quas, maxime, quaerat porro totam, dolore.
+                        </p>
+                    </li>
+                    <li><img src="img/Layer%202.png" alt="plus">
+                        
+                        <p>
+                            Delectus dolorem vero quae beatae quasi dolor deserunt iste amet atque, impedit iure placeat, ullam. Reprehenderit aliquam, nemo cum velit ratione perferendis quas, maxime, quaerat porro totam, dolore.
+                        </p>
+                    </li>
+                    <li><img src="img/Layer%202.png" alt="plus">
+                        
+                        <p>
+                            Delectus dolorem vero quae beatae quasi dolor deserunt iste amet atque, impedit iure placeat, ullam. Reprehenderit aliquam, nemo cum velit ratione perferendis quas, maxime, quaerat porro totam, dolore.
+                        </p>
+                    </li>
+                </ul>
+                
+                <div class="cls"></div>
+            </section>
+            
+            <section id="screenshots">
+                <h1>Screenshots</h1>
+                
+                <article>
+                    <div>
+                        <!-- Placeholder for the image -->
+                    </div>
+                    
+                    <h2>The Description of the image</h2>
+                    
+                    <p>
+                        Pariatur iure ab sunt nesciunt, quibusdam odio iste cumque itaque, ipsa vel exercitationem ullam quos aut nostrum cupiditate fuga quaerat quam animi dolores. Sequi itaque, unde perferendis nemo.
+                    </p>
+                </article>
+                
+                <article>
+                    <div>
+                        <!-- Placeholder for the image -->
+                    </div>
+                    
+                    <h2>The Description of the image</h2>
+                    
+                    <p>
+                        Pariatur iure ab sunt nesciunt, quibusdam odio iste cumque itaque, ipsa vel exercitationem ullam quos aut nostrum cupiditate fuga quaerat quam animi dolores. Sequi itaque, unde perferendis nemo.
+                    </p>
+                </article>
+                
+                <article>
+                    <div>
+                        <!-- Placeholder for the image -->
+                    </div>
+                    
+                    <h2>The Description of the image</h2>
+                    
+                    <p>
+                        Pariatur iure ab sunt nesciunt, quibusdam odio iste cumque itaque, ipsa vel exercitationem ullam quos aut nostrum cupiditate fuga quaerat quam animi dolores. Sequi itaque, unde perferendis nemo.
+                    </p>
+                </article>
+                
+                <article>
+                    <div>
+                        <!-- Placeholder for the image -->
+                    </div>
+                    
+                    <h2>The Description of the image</h2>
+                    
+                    <p>
+                        Pariatur iure ab sunt nesciunt, quibusdam odio iste cumque itaque, ipsa vel exercitationem ullam quos aut nostrum cupiditate fuga quaerat quam animi dolores. Sequi itaque, unde perferendis nemo.
+                    </p>
+                </article>
+                
+                <div class="cls"></div>
+            </section>
+            
+            <section id="reviews">
+                <h1>Reviews</h1>
+                
+                <article>
+                    <div class="img">
+                        <!-- Placeholder for the circle image -->
+                    </div>
+                    
+                    <div id="textbox">
+                        <p>
+                            Porro officia cumque sint deleniti nemo facere rem vitae odit inventore cum odio, iste quia doloribus autem aperiam nulla ea neque reprehenderit. Libero doloribus, possimus officiis sapiente necessitatibus commodi consectetur?
+                        </p>
+
+                        <h3>
+                            Lourens S.
+                        </h3>
+                    </div>
+                </article>
+                
+                <article>
+                    <div class="img">
+                        <!-- Placeholder for the circle image -->
+                    </div>
+                    
+                    <div id="textbox">
+                        <p>
+                            Porro officia cumque sint deleniti nemo facere rem vitae odit inventore cum odio, iste quia doloribus autem aperiam nulla ea neque reprehenderit. Libero doloribus, possimus officiis sapiente necessitatibus commodi consectetur?
+                        </p>
+
+                        <h3>
+                            Lourens S.
+                        </h3>
+                    </div>
+                </article>
+                
+                <article>
+                    <div class="img">
+                        <!-- Placeholder for the circle image -->
+                    </div>
+                    
+                    <div id="textbox">
+                        <p>
+                            Porro officia cumque sint deleniti nemo facere rem vitae odit inventore cum odio, iste quia doloribus autem aperiam nulla ea neque reprehenderit. Libero doloribus, possimus officiis sapiente necessitatibus commodi consectetur?
+                        </p>
+
+                        <h3>
+                            Lourens S.
+                        </h3>
+                    </div>
+                </article>
+                
+                <article>
+                    <div class="img">
+                        <!-- Placeholder for the circle image -->
+                    </div>
+                    
+                    <div id="textbox">
+                        <p>
+                            Porro officia cumque sint deleniti nemo facere rem vitae odit inventore cum odio, iste quia doloribus autem aperiam nulla ea neque reprehenderit. Libero doloribus, possimus officiis sapiente necessitatibus commodi consectetur?
+                        </p>
+
+                        <h3>
+                            Lourens S.
+                        </h3>
+                    </div>
+                </article>
+                
+                <div class="cls"></div>
+            </section>
+            
+            <section id="buy">
+                <h1>Buy now</h1>
+                
+                <div>
+                    <?php
+                        require('req/list.php');
+                        foreach($list as $ls):
+                    ?>
+
+                            <section>
+                                <h1>
+                                    <?php
+                                        echo $ls['name'];
+                                    ?>
+                                </h1>
+
+                                <h2>
+                                    <?php
+                                        echo '$';
+                                        echo $ls['price'];
+                                    ?>
+                                </h2>
+
+                                <ul>
+                                    <?php
+                                        for ($i=1; $i<=$ls['bullets']; $i++):
+                                    ?>
+                                        <li><?php echo $texts[$i-1] ?></li>
+                                    <?php
+                                        endfor;
+                                    ?>
+                                </ul>
+                                
+                                <a href="#">
+                                    BUY
+                                </a>
+                            </section>
+                    <?php endforeach ?>
+                </div>
+                
+                <div class="cls"></div>
+            </section>
+        </main>
+        
+        <?php require('req/footer.php'); ?>
+    </body>
+</html>
---
+++ req/footer.php
@@ -0,0 +1,53 @@
+<footer>
+    <section>
+        <h1>Contacts</h1>
+        
+        <form>
+            <input type="text" placeholder="Your name:">
+            <input type="text" placeholder="Your email:">
+            <textarea name="" id="" cols="30" rows="10" placeholder="Your message:"></textarea>
+            
+            <input type="submit" value="Submit" id="submit">
+        </form>
+        
+        <div class="contacts">
+            <ul>
+                <li>
+                    <img src="../img/skype%20icon.png" alt="skype">
+                    here_your_login_skype
+                </li>
+                <li>
+                    <img src="../img/icq%20icon.png" alt="icq">
+                    279679659
+                </li>
+                <li>
+                    <img src="../img/email%20icon.png" alt="email">
+                    psdhtmlcss@mail.ru
+                </li>
+                <li>
+                    <img src="../img/phone%20icon.png" alt="phone">
+                    80 00 4568 55 55
+                </li>
+                <li>                    
+                    <a href="#">
+                        <img src="../img/facebook.png" alt="facebook">
+                    </a>
+                    <a href="#">
+                        <img src="../img/google.png" alt="google">
+                    </a>
+                    <a href="#">
+                        <img src="../img/instagram.png" alt="instagram">
+                    </a>
+                    <a href="#">
+                        <img src="../img/twitter.png" alt="twitter">
+                    </a>
+                    <a href="#">
+                        <img src="../img/yt.png" alt="youtube">
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </section>
+    
+    <div class="cls"></div>
+</footer>
