65264ffb05f42c3eefe4061bdeab380df06f72de
SugarF0x <malion.torchwood@yandex.ru>
2018-11-25 21:17:46 +0300

    Replaced all contacts images with FontAwesome and fixed a few alignment issues

--- req/footer.php
+++ req/footer.php
@@ -13,36 +13,44 @@
         <div class="contacts">
             <ul>
                 <li>
-                    <img src="../img/skype%20icon.png" alt="skype">
-                    here_your_login_skype
+                    <i class="fab fa-skype"></i>
+                    <p>
+                        here_your_login_skype
+                    </p>
                 </li>
                 <li>
-                    <img src="../img/icq%20icon.png" alt="icq">
-                    279679659
+                    <i class="fab fa-facebook-messenger"></i>
+                    <p>
+                        279679659
+                    </p>
                 </li>
                 <li>
-                    <img src="../img/email%20icon.png" alt="email">
-                    psdhtmlcss@mail.ru
+                    <i class="fas fa-at"></i>
+                    <p>
+                        psdhtmlcss@mail.ru
+                    </p>
                 </li>
                 <li>
-                    <img src="../img/phone%20icon.png" alt="phone">
-                    80 00 4568 55 55
+                    <i class="fas fa-phone"></i>
+                    <p>
+                        80 00 4568 55 55
+                    </p>
                 </li>
                 <li>                    
                     <a href="#">
-                        <img src="../img/facebook.png" alt="facebook">
+                        <i class="fab fa-facebook-f"></i>
                     </a>
                     <a href="#">
-                        <img src="../img/google.png" alt="google">
+                        <i class="fab fa-google"></i>
                     </a>
                     <a href="#">
-                        <img src="../img/instagram.png" alt="instagram">
+                        <i class="fab fa-linkedin-in"></i>
                     </a>
                     <a href="#">
-                        <img src="../img/twitter.png" alt="twitter">
+                        <i class="fab fa-twitter"></i>
                     </a>
                     <a href="#">
-                        <img src="../img/yt.png" alt="youtube">
+                        <i class="fab fa-youtube"></i>
                     </a>
                 </li>
             </ul>
